fd5fc23c64a28a986d7f3aec69ed89eac2cca4a5
devchaturved <94732026+devchaturved@users.noreply.github.com>
2025-05-05 01:20:29 +0530

    Add files via upload

---
+++ DL_codes/DL-Copy/Miniproject Colourizer/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+ Training Dataset is computationally expensive and dataset required is quite large.
+#  Hence we will use Pre-trained Models.
+#  Model files we used:
+#  1. colorization_deploy_v2_prototxt.
+#  2. pts_in_hull.npy.
+#  3. colorization_release_v2.caffemodel [https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1]
+#  Place the caffe model in the colorize folder.
+# '''
